46f4a01679b47a81b4082329d0d7178cfa6941d5
perhapsOtto <143024479+perhapsOtto@users.noreply.github.com>
2025-05-29 15:10:55 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/me.jpeg") {
+    myImage.setAttribute("src", "images/goat.JPG");
+  } else {
+    myImage.setAttribute("src", "images/me.jpeg");
+
+  }
+});
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Hello ${myName}!`;
+    }
+  }
+  
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Hello ${storedName}!`;
+}
+
+myButton.addEventListener("click", () => {
+    setUserName();
+});
+  
